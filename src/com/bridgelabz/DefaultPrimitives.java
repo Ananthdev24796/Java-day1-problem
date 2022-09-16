ab5c556eb0c66e9bf4724f54a54fc8673771996a
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-16 11:21:47 +0530

    Added:default primitives program in day one problem

---
+++ src/com/bridgelabz/DefaultPrimitives.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author Iam_A
+ *
+ */
+public class DefaultPrimitives {
+
+	/**
+	 * @param args
+	 */
+	
+	static boolean bn;
+	static double d;
+	static float f;
+	static char ch;
+	static int i;
+	static byte b;
+	static long	l;
+	static short s;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("boolean:"+ bn + "\n" +"double:" + d + "\n" + "float:" + f + "\n" + "char:" + ch +"\n"+ "int:" + i + "\n" + "byte:" +b +"\n"+ "long:" + l + "\n" + "short:"+ s + "\n");
+
+	}
+
+}
