5586ae755c67f67e226226a71ecafc8d8c6c6621
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-16 09:46:08 +0530

    Added:Day 1 problems

---
+++ src/com/bridgelabz/DisplayAMessage.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author Iam_A
+ *
+ */
+public class DisplayAMessage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s ="Hai Everyone ";
+		System.out.println(s);
+
+	}
+
+}
