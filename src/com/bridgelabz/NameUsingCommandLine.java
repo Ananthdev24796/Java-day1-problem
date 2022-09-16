72d9aab5e99fb6068b4876fbb7b91464639a72b4
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-16 11:26:30 +0530

    Added:Name using commandLine program in day 1 problems

---
+++ src/com/bridgelabz/NameUsingCommandLine.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * @author Iam_A
+ *
+ */
+public class NameUsingCommandLine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the input");
+		String input =sc.nextLine();
+		System.out.println(input);
+	}
+
+}
